52f2b9e2059ebffe6dd023d5b1112f98b7af4529
christopher-palmieri <christopher.palmieri@nmshealth.com>
2025-07-08 13:42:47 -0400

    Update vapi-conference.js

--- api/twilio/vapi-conference.js
+++ api/twilio/vapi-conference.js
@@ -1,16 +1,24 @@
+// First, update your vapi-conference.js endpoint
 // /api/twilio/vapi-conference.js
 export default async function handler(req, res) {
   const { conferenceId } = req.query;
   
   console.log('🎤 VAPI joining conference:', conferenceId);
   
-  // VAPI joins conference NOT muted
+  const baseUrl = process.env.WEBHOOK_URL || 
+                 process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 
+                 'http://localhost:3000';
+  
+  // VAPI joins conference with a callback when it joins
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
     <Response>
       <Dial>
         <Conference 
           startConferenceOnEnter="true"
           endConferenceOnExit="false"
+          statusCallback="${baseUrl}/api/twilio/conference-participant-status?conferenceId=${conferenceId}"
+          statusCallbackEvent="join"
+          statusCallbackMethod="POST"
           beep="false">
           ${conferenceId}
         </Conference>
