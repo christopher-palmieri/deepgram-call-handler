d6f264747dcace93437e614a510164a45a0edea9
christopher-palmieri <christopher.palmieri@nmshealth.com>
2025-07-17 06:53:01 -0400

    Update direct-vapi-test.js

--- api/twilio/direct-vapi-test.js
+++ api/twilio/direct-vapi-test.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
     <Response>
       <Say>Testing VAPI connection with variables. Connecting now.</Say>
       <Dial>
-        <Sip>sip:brandon-call-for-kits@sip.vapi.ai?x-customername=TestTest</Sip>
+        <Sip>sip:brandon-call-for-kits@sip.vapi.ai?x-customername=Indiana%20Jones</Sip>
       </Dial>
     </Response>`;
   
