a01bfbd09736c7fab877b106c7efd947dea04d92
christopher-palmieri <christopher.palmieri@nmshealth.com>
2025-07-28 15:24:25 -0400

    Update preclassify-twiml.js

--- api/twilio/preclassify-twiml.js
+++ api/twilio/preclassify-twiml.js
@@ -112,6 +112,15 @@ export default async function handler(req, res) {
       year: 'numeric',
       month: 'long',
       day: 'numeric'
+      }    
+    if (pendingCallData.appointment_time) {
+      const readableAppt = new Intl.DateTimeFormat('en-US', {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true     
     }).format(new Date(pendingCallData.employee_dob));
     customHeaders['employee_dob'] = readableDob;
     }
