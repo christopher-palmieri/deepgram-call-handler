ce3445cf5e50a68a1db6434e55a1aede99390aa9
christopher-palmieri <christopher.palmieri@nmshealth.com>
2025-09-29 15:19:34 -0400

    Update get-pending-call.js

--- api/twilio/get-pending-call.js
+++ api/twilio/get-pending-call.js
@@ -29,7 +29,7 @@ export default async function handler(req) {
   }
 
   const supabaseUrl = 'https://ixbuuvggqzscdsfkzrri.supabase.co/rest/v1/pending_calls';
-  const supabaseApiKey = process.env.SUPABASE_ANON_KEY;
+  const supabaseApiKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
   const supabaseRes = await fetch(`${supabaseUrl}?id=eq.${id}&select=id,employee_name,exam_id,employee_dob,client_name,appointment_time,type_of_visit,clinic_name,clinic_provider_address,clinic_scheduling_rep,procedures`, {
   headers: {
